a5e215131722be366f2a8d003935e216f8433845
Vedant Chainani <vedantchainani1084@gmail.com>
2025-04-09 18:53:10 +0530

    chore: add tests for hmac

--- tests/lib.test.ts
+++ tests/lib.test.ts
@@ -1,12 +1,25 @@
 import { beforeAll, describe, expect, it } from 'vitest';
 
-import { Prover } from '@zkpersona/noir-helpers';
+import { BoundedVec, Prover, Str, U8, toJSON } from '@zkpersona/noir-helpers';
 
-import circuit from '../target/lib_test.json' assert { type: 'json' };
+import circuit from '../target/hmac_sha256_example.json' assert {
+  type: 'json',
+};
 
 import type { CompiledCircuit } from '@noir-lang/noir_js';
 
-const inputs = { x: '1', y: '2' };
+const keyBytes = new Str('secret').asBytes();
+const key = new BoundedVec(keyBytes.length, () => new U8(0));
+key.extendFromArray(keyBytes);
+
+const messageBytes = new Str('hello_world').asBytes();
+const message = new BoundedVec(messageBytes.length, () => new U8(0));
+message.extendFromArray(messageBytes);
+
+const inputs = {
+  key,
+  message,
+};
 
 describe('Circuit Proof Verification', () => {
   let prover: Prover;
@@ -16,14 +29,16 @@ describe('Circuit Proof Verification', () => {
   });
 
   it('should prove using honk backend', async () => {
-    const proof = await prover.fullProve(inputs, { type: 'honk' });
+    const parsedInputs = toJSON(inputs);
+    const proof = await prover.fullProve(parsedInputs, { type: 'honk' });
     const isVerified = await prover.verify(proof, { type: 'honk' });
 
     expect(isVerified).toBe(true);
   });
 
   it('should prove using plonk backend', async () => {
-    const proof = await prover.fullProve(inputs, { type: 'plonk' });
+    const parsedInputs = toJSON(inputs);
+    const proof = await prover.fullProve(parsedInputs, { type: 'plonk' });
     const isVerified = await prover.verify(proof, { type: 'plonk' });
 
     expect(isVerified).toBe(true);
